ef840566e66c08449a77c015ea0641d2d288e371
OzcanGuler <gulerozcan818@gmail.com>
2022-07-21 15:04:11 +0300

    Completed

---
+++ ChessBoardGUI/ChessBoardGUI/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChessBoardGUI
+{
+    public partial class Form1 : Form
+    {
+        static Board myBoard = new Board(8);
+        public Button[,] btnGrid = new Button[myBoard.Size, myBoard.Size];
+        public Form1()
+        {
+            InitializeComponent();
+            pupulateGrid();
+        }
+
+        private void pupulateGrid()
+        {
+            int buttonSize = panel1.Width / myBoard.Size;
+            panel1.Height = panel1.Width;
+
+            for (int i = 0; i < myBoard.Size; i++)
+            {
+                for (int j = 0; j < myBoard.Size; j++)
+                {
+                    btnGrid[i, j] = new Button();
+
+                    btnGrid[i, j].Height = buttonSize;
+                    btnGrid[i, j].Width = buttonSize;
+
+                    btnGrid[i, j].Click += Grid_Button_Click;
+
+                    panel1.Controls.Add(btnGrid[i, j]);
+
+                    btnGrid[i, j].Location = new Point(i * buttonSize, j * buttonSize);
+
+                    btnGrid[i, j].Text = i + "|" + j;
+                    btnGrid[i, j].Tag = new Point(i, j);
+                }
+            }
+        }
+        private void Grid_Button_Click(object sender, EventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            Point location = (Point)clickedButton.Tag;
+
+            int x = location.X;
+            int y = location.Y;
+
+            Cell currentCell = myBoard.theGrid[x,y];
+
+            myBoard.MarkNextLegalMoves(currentCell,comboBox1.Text);
+
+            for (int i = 0; i < myBoard.Size; i++)
+            {
+                for (int j = 0; j < myBoard.Size; j++)
+                {
+                    btnGrid[i, j].Text = "";
+                    if(myBoard.theGrid[i, j].LegalNextMove == true)
+                    {
+                        btnGrid[i, j].Text = "Legal";
+                    }
+                    else if (myBoard.theGrid[i, j].CurrentlyOccupied == true)
+                    {
+                        btnGrid[i, j].Text = comboBox1.Text;
+                    }
+                }
+            }
+        }
+    }
+}
